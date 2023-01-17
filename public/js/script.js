0bcd1582bc303604f09521be0d0d2e1d5a153683
lucas <lucas@gmail.com>
2023-01-17 16:52:09 -0300

    att git

---
+++ public/js/script.js
@@ -0,0 +1,82 @@
+function select() {
+    let file = document.getElementById('flImage');
+    file.click();
+}
+
+function atualiza() {
+    let photo = document.getElementById('imgFoto');
+    let file = document.getElementById('flImage');
+    // Sem essa verificação, ele irá dar erro quando o usuário clicar em cancelar
+    // pois enviará uma "imagem" vazia
+    if (file.files.length == 0) {
+    return;
+    }
+    // Inicializando a função que pega o caminho da imagem
+    let reader = new FileReader();
+    // Está pegando o caminho da imagem
+    reader.readAsDataURL(file.files[0]);
+    // Coloca o caminho da imagem no Source da tag IMG
+    reader.onload = () => {
+        photo.src = reader.result
+    }
+};
+
+function cadAluno(){
+    let aluno = document.getElementById('newAluno');
+    let sala = document.getElementById('newSala');
+    let fundo = document.querySelector('.content');
+ 
+    if(aluno.classList.contains("hidden")){
+        aluno.classList.remove("hidden");
+        aluno.classList.add("show");
+        fundo.classList.add("blur");
+    }
+    if(sala.classList.contains("show")){
+        sala.classList.remove("show");
+        sala.classList.add("hidden");
+    }
+}
+
+function cadSala(){
+    let aluno = document.getElementById('newAluno');
+    let sala = document.getElementById('newSala');
+    let fundo = document.querySelector('.content');
+ 
+    if(aluno.classList.contains("show")){
+        aluno.classList.remove("show");
+        aluno.classList.add("hidden");
+    }
+    if(sala.classList.contains("hidden")){
+        sala.classList.remove("hidden");
+        sala.classList.add("show");
+        fundo.classList.add("blur");
+    }
+}
+
+function voltaTela(){
+    let aluno = document.getElementById('newAluno');
+    let sala = document.getElementById('newSala');
+    let fundo = document.querySelector('.content');
+    
+    if(aluno.classList.contains("show")){
+        aluno.classList.remove("show");
+        aluno.classList.add("hidden");
+        fundo.classList.remove("blur");
+    }
+    if(sala.classList.contains("show")){
+        sala.classList.remove("show");
+        sala.classList.add("hidden");
+        fundo.classList.remove("blur");
+    }
+}
+
+function Editar(){
+    
+    let nome = document.getElementById();
+
+}
+
+function Salvar(){
+
+    let fundo = document.querySelector('.content');
+}
